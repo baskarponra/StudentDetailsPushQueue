e3a6c942ae60d2af70db1c2a53c162faa24e5860
BaskaranP <2020mt93207@wilp.bits-pilani.ac.in>
2021-10-21 23:16:41 +0530

    Student Details Queue

---
+++ ReportDataCollector.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using Plain.RabbitMQ;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReportService
+{
+    public class ReportDataCollector : IHostedService
+    {
+        private const int DEFAULT_QUANTITY = 100;
+        private readonly ISubscriber subscriber;
+        private readonly IMemoryReportStorage memoryReportStorage;
+
+        public ReportDataCollector(ISubscriber subscriber, IMemoryReportStorage memoryReportStorage)
+        {
+            this.subscriber = subscriber;
+            this.memoryReportStorage = memoryReportStorage;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            subscriber.Subscribe(ProcessMessage);
+            return Task.CompletedTask;
+        }
+
+        private bool ProcessMessage(string message, IDictionary<string, object> headers)
+        {
+            
+            return true;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
